93bf2eaf5ed31e9a1faa82fe416b5c72b1d65cc9
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2023-12-07 15:17:09 +0700

    Style Registration Page

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import Button from '../elements/button';
+//import Button from '../elements/button';
 import axios from 'axios';
 import PATH from '@/routes/paths';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuthContext } from '@/contexts/AuthContext';
 import { useRouter } from 'next/router';
+import { Button, FloatingLabel, Checkbox, Label, TextInput } from 'flowbite-react';
+import { HiMail } from 'react-icons/hi';
+
 
 export default function RegisterForm() {
   const [formData, setFormData] = useState({
@@ -20,9 +23,13 @@ export default function RegisterForm() {
   const { saveTokens } = useAuthContext();
   const router = useRouter();
 
-  function handleChange(target) {
-    setData({ ...data, [target.id]: target.value });
-  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   async function handleRegister() {
     if (isLoading === true) {
@@ -82,68 +89,76 @@ export default function RegisterForm() {
   return (
     <>
       <ToastContainer />
-      <form className="flex h-96 w-80 flex-col justify-around rounded-xl p-4">
+      <form className="flex h-full max-h-screen w-full max-w-screen-sm gap-5 flex-col justify-around rounded-xl p-4">
         <h1 className="text-x text-center text-xl font-bold">
           Registration Form
         </h1>
         <div>
-          <div className="mb-4">
-            <label htmlFor="first_name" className="block">
-              First Name:
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              className="w-full"
-              onChange={(e) => handleChange(e.target)}
-            />
+          <div className="mb-4 block">
+            <div className="mb-2 block">
+              <Label htmlFor="first_name" value="Your First Name" />
+            </div>
+            <TextInput 
+              id="first_name" 
+              type="text" 
+              name="first_name"
+              value={formData.first_name}
+              onChange={handleChange}
+              required />
           </div>
-          <div className="mb-4">
-            <label htmlFor="last_name" className="block">
-              Last Name:
-            </label>
-            <input
-              type="text"
-              id="Last_name"
-              className="w-full"
-              onChange={(e) => handleChange(e.target)}
-            />
+          <div className="mb-4 block">
+            <div className="mb-2 block">
+              <Label htmlFor="last_name" value="Your Last Name" />
+            </div>
+            <TextInput 
+              id="last_name" 
+              type="text" 
+              name="last_name"
+              value={formData.last_name}
+              onChange={handleChange}
+              required />
           </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full"
-              onChange={(e) => handleChange(e.target)}
-            />
+          <div className="mb-4 block">
+              <div className="mb-2 block">
+                <Label htmlFor="email" value="Your email" />
+              </div>
+              <TextInput 
+                id="email" 
+                type="email" 
+                icon={HiMail} 
+                placeholder="name@mail.com" 
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required 
+              />
           </div>
-          <div>
-            <label htmlFor="password" className="block">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full"
-              onChange={(e) => handleChange(e.target)}
-            />
+          <div className="mb-4 block">
+            <div className="mb-2 block">
+              <Label htmlFor="password" value="Your password" />
+            </div>
+            <TextInput 
+              id="password" 
+              type="password" 
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required />
           </div>
-          <div>
-            <label htmlFor="confirm_password" className="block">
-              Confirm Password:
-            </label>
-            <input
-              type="confirmPassword"
-              id="confirm_password"
-              className="w-full"
-              onChange={(e) => handleChange(e.target)}
-            />
+          <div className="mb-4 block">
+            <div className="mb-2 block">
+              <Label htmlFor="confirm_password" value="Confirm password" />
+            </div>
+            <TextInput 
+              id="confirm_password" 
+              type="password" 
+              name="confirm_password"
+              value={formData.confirm_password}
+              onChange={handleChange}
+              required />
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center place-content-center ">
           <Button onClick={handleRegister}>Register</Button>
         </div>
       </form>
