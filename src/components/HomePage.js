e3c307e9f4bf796810638600560aaa9c05bb0bfd
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2023-12-11 12:21:25 +0700

    Finish Homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,5 @@
 import { ToastContainer } from 'react-toastify';
-import { Accordion } from 'flowbite-react';
-import Link from 'next/link';
+import { List, Blockquote } from 'flowbite-react';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function HomePage() {
@@ -9,78 +8,54 @@ export default function HomePage() {
       <ToastContainer />
       <div className="container">
         <div className="my-5">
-          <h1>EVENTIFY</h1>
-          <p>Ini isi highlight web app nya</p>
+          <h1 className="mb-5 text-center mx-5 ">Eventify</h1>
         </div>
 
         <div className="my-5">
-          <Accordion>
-            <Accordion.Panel>
-              <Accordion.Title>Apa itu Eventify?</Accordion.Title>
-              <Accordion.Content>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Eventify didesain untuk meningkatkan produktivitas Anda dengan
-                  menyediakan plataform untuk menyimpan, merencanakan, dan
-                  mengoragnisir acara dan aktivitas Anda. Dengan Eventify anda
-                  dapat mengoragnisir acara dan aktivitas Anda menggunakan
-                  metode PARA
-                </p>
-              </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-              <Accordion.Title>Apa itu Metode PARA?</Accordion.Title>
-              <Accordion.Content>
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Metode PARA adalah metode yang dibuat oleh Forte. Dalam
-                  bukunya, Forte mengatakan bahwa metode PARA adalah sistem yang
-                  sederhana, komprehensif, dan fleksibel untuk mengatur semua
-                  jenis informasi di semua platform digital. metode PARA terbagi
-                  menjadi empat kategori yaitu Projects, Areas, Resources, dan
-                  Archives.
-                </p>
-                <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                  <li>Projects</li>
-                  <p className="mb-2 list-disc text-gray-500 dark:text-gray-400">
-                    Merupakan acara atau aktivitas yang memiliki tujuan untuk
-                    diselesaikan dengan tenggat waktu tertentu
-                  </p>
-                  <li>Areas</li>
-                  <p className="list-disctext-gray-500 mb-2 dark:text-gray-400">
-                    Merupakan aktivitas yang berperan penting dalam kehidupan
-                    kita, bertindak sebagai standar yang dipertahankan, dan
-                    berkelanjutan (tanpa ada batas waktu tertentu)
-                  </p>
-                  <li>Resources</li>
-                  <p className="mb-2 list-disc text-gray-500 dark:text-gray-400">
-                    Merupakan aktivitas yang cukup menarik perhatian kita,
-                    tetapi bukan bagian dari tanggung jawab kita
-                  </p>
-                  <li>Archives</li>
-                  <p className="mb-2 list-disc text-gray-500 dark:text-gray-400">
-                    Merupakan acara atau aktivitas yang telah selesai kita
-                    lakukan atau tidak lagi termasuk pada tiga kategori
-                    sebelumnya
-                  </p>
-                </ul>
-              </Accordion.Content>
-            </Accordion.Panel>
-          </Accordion>
+          <Blockquote className="mx-5 mb-2 text-gray-700 dark:text-gray-400 text-base text-center md:text-3xl">
+            Eventify didesain untuk meningkatkan produktivitas Anda dengan
+            menyediakan plataform untuk menyimpan, merencanakan, dan
+            mengoragnisir acara dan aktivitas Anda. Dengan Eventify anda
+            dapat mengoragnisir acara dan aktivitas Anda menggunakan
+            metode PARA.
+          </Blockquote>
         </div>
-
-        <div className="my-5">
-          <h2>Mulai Sekarang</h2>
-          <p>
-            Untuk mulai menggunakan Eventify,
-            <span>
-              {' '}
-              <Link
-                href="/auth/register"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Daftar sekarang
-              </Link>
-            </span>
+        <div className="my-5 mt-10">
+          <div>
+            <h2 className="mx-5 mb-2 text-gray-800 dark:text-gray-400">Apa itu Metode PARA?</h2>
+          </div>
+          <p className="mx-5 mb-2 text-gray-500 dark:text-gray-400 text-base md:text-xl">
+            Metode PARA adalah metode yang dibuat oleh Forte. Dalam
+            bukunya, Forte mengatakan bahwa metode PARA adalah sistem yang
+            sederhana, komprehensif, dan fleksibel untuk mengatur semua
+            jenis informasi di semua platform digital. metode PARA terbagi
+            menjadi empat kategori yaitu Projects, Areas, Resources, dan
+            Archives.
           </p>
+          <List className="list-disc pl-5 mx-5 mb-2 text-gray-500 dark:text-gray-400 text-base md:text-xl">
+            <List.Item>Projects</List.Item>
+            <p className="mb-2 list-disc text-gray-500 dark:text-gray-400 text-xl">
+              Merupakan acara atau aktivitas yang memiliki tujuan untuk
+              diselesaikan dengan tenggat waktu tertentu
+            </p>
+            <List.Item>Areas</List.Item>
+            <p className="list-disctext-gray-500 mb-2 dark:text-gray-400 text-xl">
+              Merupakan aktivitas yang berperan penting dalam kehidupan
+              kita, bertindak sebagai standar yang dipertahankan, dan
+              berkelanjutan (tanpa ada batas waktu tertentu)
+            </p>
+            <List.Item>Resources</List.Item>
+            <p className="mb-2 list-disc text-gray-500 dark:text-gray-400 text-xl">
+              Merupakan aktivitas yang cukup menarik perhatian kita,
+              tetapi bukan bagian dari tanggung jawab kita
+            </p>
+            <List.Item>Archives</List.Item>
+            <p className="mb-2 list-disc text-gray-500 dark:text-gray-400 text-xl">
+              Merupakan acara atau aktivitas yang telah selesai kita
+              lakukan atau tidak lagi termasuk pada tiga kategori
+              sebelumnya
+            </p>
+          </List>
         </div>
       </div>
     </>
